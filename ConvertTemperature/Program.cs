9582f5715fff98acd280ea28ab229f0e52fc6184
ZacharyLi2013 <zacharyli312640@gmail.com>
2020-08-29 09:45:52 -0700

    convert temperature

---
+++ ConvertTemperature/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConvertTemperature
+{
+    class ConvertTemperature
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("\nenter C temperature: ");
+            float C = float.Parse(System.Console.ReadLine());
+            float F = (C * 9/5) + 32;
+            System.Console.WriteLine("the temperature in F is: "  + F);
+        }
+    }
+}
